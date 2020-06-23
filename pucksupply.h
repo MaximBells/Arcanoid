1b3076a38be101d34aacd3be558adc82944a5caa
artem0679 <55602618+artem0679@users.noreply.github.com>
2020-06-23 13:47:19 +0300

    Update pucksupply.h

--- pucksupply.h
+++ pucksupply.h
@@ -1,11 +1,26 @@
-#ifndef PUCKSUPPLY_H
-#define PUCKSUPPLY_H
-#include "puck.h"
+#pragma once
 
-class PuckSupply
-{
-public:
-    PuckSupply();
+#include <QImage>
+#include <QRect>
+class PuckSupply {
+
+  public:
+    PuckSupply(int, int);
+    ~PuckSupply();
+
+  public:
+
+
+
+    QRect getRect();
+
+    QImage & getImage();
+
+  private:
+
+    QImage image;
+    QRect rect;
+    static const int INITIAL_X = 220;
+    static const int INITIAL_Y = 30;
+
 };
-
-#endif // PUCKSUPPLY_H
